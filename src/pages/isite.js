d04b0f76db49b619ce540da7939130308d81a0ef
Josh Ribakoff <josh.ribakoff@gmail.com>
2018-08-18 01:52:52 -0700

    add mobile video

--- src/pages/isite.js
+++ src/pages/isite.js
@@ -2,5 +2,7 @@ import React from "react";
 import Link from "gatsby-link";
 import styled from 'styled-components'
 
-export default () => 
+export default () => <div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/TQfICz8VlP0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/gL9bcDHn2Rk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+</div>
