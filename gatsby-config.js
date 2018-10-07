0a31362e66b1a44ae757aad2ea794c6f8d3588af
Josh Ribakoff <josh.ribakoff@gmail.com>
2018-10-07 14:56:12 -0700

    adds youtube source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,14 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography.js`
       }
-    }
+    },
+    {
+      resolve: `gatsby-source-youtube`,
+      options: {
+        channelId: 'UCjRZ1UuCCDutlIk_skNvSlw',
+        apiKey: 'AIzaSyAPBMeV1x-3FAal-XE5J63tNaNSC-b35dk',
+        maxVideos: 50 // Defaults to 50
+      },
+    },
   ]
 };
